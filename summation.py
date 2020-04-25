2bd70be5dcc40d24be0c6edc35278b03847da118
ASBER <ASBER@vestas.com>
2020-04-25 07:19:46 +0200

    general polishing

--- summation.py
+++ summation.py
@@ -2,42 +2,64 @@ import random
 import math
 import os
 import re
+import time
 
 max_trials = 5
+sleep_time = 2
+
+positive_messages = [
+    ':-)',
+    'Great!!',
+    'Nice!!',
+    'Awesome!!',
+    'Well done!!',
+    'Keep it up!!',
+    'I like it!!',
+    'Cheers!!',
+]
+
+
+def seek_input(i1, i2):
+    trial = 0
+    guessed = False
+    for  trial in range(1, max_trials+1):
+        answer = input(f"{i1} + {i2} = ")
+        answer = int(re.sub('[^0-9]','', answer))
+    
+        if answer == i1 + i2:
+            guessed = True
+            break
+        if trial < max_trials:
+            print(f"Nope... try again! ({trial} of {max_trials})")
+        else:
+            input(f"Ops... The correct answer was {i1} + {i2} = {i1 + i2}")
+
+    return guessed
 
 def main():
-    os.system('cls')
     
     while True:
+        os.system('cls')
         i1 = random.randint(0,11)
         i2 = random.randint(0,25)
-
-        trial = 0
-        while  trial < max_trials:
-            answer = input(f"{i1} + {i2} = ")
-            if answer == 'e':
-                exit()
-            else:
-                answer = int(re.sub('[^0-9]','', answer))
-            
-            if answer == i1 + i2:
-                guessed = True
-                break
-            print("Thats's not it... try again!")
-            trial += 1
-        else:
-            guessed = False
-
-        if guessed:
-            input(f"Correct!!! ")
-        else:
-            input(f"Ops, those where not the answers... The correct answer was {i1} + {i2} = {i1 + i2} \n")
+        guessed = seek_input(i1, i2)
         
-        os.system('cls')
+        if guessed:
+            message = f"Correct!!! {random.choice(positive_messages)}"
+            if sleep_time>0:
+                print(message)
+                time.sleep(sleep_time)
+            else:
+                input(message)
         
 
 
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except:
+        pass
+    finally:
+        os.system('cls')
