7c6402be3135107b8d7786a0d55240f5ec8f61c2
ASBER <ASBER@vestas.com>
2020-04-25 08:06:23 +0200

    add sessions and summary

--- summation.py
+++ summation.py
@@ -3,9 +3,12 @@ import math
 import os
 import re
 import time
+from collections import OrderedDict
+
 
 max_trials = 5
 sleep_time = 2
+session_size = 10
 
 positive_messages = [
     ':-)',
@@ -23,34 +26,62 @@ def seek_input(i1, i2):
     trial = 0
     guessed = False
     for  trial in range(1, max_trials+1):
-        answer = input(f"{i1} + {i2} = ")
+        answer = input(f"  {i1} + {i2} = ")
         answer = int(re.sub('[^0-9]','', answer))
     
         if answer == i1 + i2:
             guessed = True
             break
         if trial < max_trials:
-            print(f"Nope... try again! ({trial} of {max_trials})")
+            print(f"Nope... try again! (attempt {trial} of {max_trials})")
         else:
-            input(f"Ops... The correct answer was {i1} + {i2} = {i1 + i2}")
+            input(f"\nOps... The correct answer was {i1} + {i2} = {i1 + i2}")
 
     return guessed
 
-def main():
-    
-    while True:
+def summary_to_str(summary):
+    os.system('cls')
+    txt = 'Here is your summary: \n \n'
+    for s in summary:
+        if s[2]:
+            guessed = 'v'
+        else:
+            guessed = 'XXX'
+        txt = (txt  + '  ' +
+               guessed.ljust(5) + 
+               str(s[0]).ljust(3) +  ' + ' + 
+               str(s[1]).ljust(3) +  ' = ' + 
+               str(s[0] + s[1])) + '\n'
+    return txt
+
+def enquiry_session():
+
+    summary = []
+    for i, question in enumerate(range(1, session_size+1)):
         os.system('cls')
+        print(f"Question {question} of {session_size}: \n")
+
         i1 = random.randint(0,11)
         i2 = random.randint(0,25)
         guessed = seek_input(i1, i2)
         
+        summary.append((i1, i2, guessed))
+
         if guessed:
-            message = f"Correct!!! {random.choice(positive_messages)}"
+            message = f"\nCorrect!!! {random.choice(positive_messages)}"
             if sleep_time>0:
                 print(message)
                 time.sleep(sleep_time)
             else:
                 input(message)
+    
+    input(summary_to_str(summary))
+
+
+def main():
+    
+    while True:
+        enquiry_session()
         
 
 
